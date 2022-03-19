ff08e9a490b305241281f32e8fde4a090509f49c
unknown <mikimikail@student.uns.ac.id>
2022-03-19 20:57:12 +0700

    mengerjakan exercise 02

---
+++ Exercise-01/1.py
@@ -0,0 +1,15 @@
+from tracemalloc import start
+
+
+start
+#program menghitung luas lingkaran dan keliling lingkaran
+print('====================')
+r=float(input('masukan jari jari lingkaran = '))
+print()
+pi = 3,14
+keliling = 2*pi*r
+luas = pi*r**2
+print ('luas lingkaran adalah =',luas)
+print ('keliling lingkaran adalah = ', keliling)
+print ('asu koe')
+print ('======================')
---
+++ Exercise-01/from tracemalloc import start.py
@@ -0,0 +1,10 @@
+from tracemalloc import start
+
+
+start
+a = int(input('masukan variabel a = '))
+b = float(input('masukan variabel b ='))
+print ('=================')
+jumlah = a + b 
+print ('=================')
+print (jumlah)
